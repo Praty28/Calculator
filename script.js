0f52186cc457ab2b9a1473037d1aa443fb1e2901
Pratyush Mendon <mendonpratyush908@gmail.com>
2021-09-01 16:21:48 +0530

    Add variables to store numbers, add an operations function to store values in variables

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
+
+/* ##########################
+    Operation Functions
+########################## */
+
+
 const add = function(a, b) {
     return a + b
 }
@@ -47,13 +54,23 @@ let operate = function (a, b, c) {
 
 
 /* ##########################
+    Variables
+########################## */
+let displayValue = '';
+let onlyNumbers = '';
+let firstValue = '';
+let secondValue = '';
+
+/* ##########################
     Button Selectors
 ########################## */
 
 const clear = document.querySelector(".clear")
 
 clear.addEventListener('click', () => { 
+    onlyNumbers = '';
     return document.querySelector('p.displayText').textContent = "";
+    
 });
 
 
@@ -61,4 +78,14 @@ const allButtons = document.querySelectorAll("button:not(#exclude)")
 
 Array.from(allButtons).forEach((el, index) => el.addEventListener('click', () => {
     document.querySelector('.displayText').textContent += allButtons[index].textContent;
+    onlyNumbers += allButtons[index].textContent.toString();
+}))
+
+const operations = document.querySelectorAll('.operatorMultiply, .operatorDivide, .operatorSubtract, .operatorAdd');
+
+Array.from(operations).forEach((el, index) => el.addEventListener('click', () => {
+    displayValue = document.querySelector('.displayText').textContent;
+    firstValue = Number(onlyNumbers);
+    console.log(firstValue);
+    document.querySelector('.displayText').textContent += operations[index].textContent;
 }))
